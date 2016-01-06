2fd6eede21b4216e90eb00b4a0238be9ace7f861
LazyTarget <peter.aslund@hotmail.com>
2016-01-06 18:11:55 +0100

    Initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		Debug.Log("LoadLevel() Name: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void Quit(){
+		Debug.Log("Quit()");
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	private static MusicPlayer _instance = null;
+
+	void Awake() {
+		if (_instance != null) {
+			// Don't create new music players if one already exists
+			Destroy(gameObject);
+		}
+		else {
+			// Don't destroy the music player when changing scenes
+			_instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
